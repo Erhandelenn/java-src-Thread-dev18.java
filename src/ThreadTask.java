89d969cbd48bd20c077bb187f61636a7286a17f6
Erhan Delen <Erhandln611@gmail.com>
2024-07-11 19:03:45 +0300

    güncelle

--- src/ThreadTask.java
+++ src/ThreadTask.java
@@ -1,39 +1,37 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ThreadTask {
+    private static final int MAX_NUMBER = 100;
 
     public static void main(String[] args) {
-        int threadCount = 10;
-        ExecutorService sabitHavuz = Executors.newFixedThreadPool(threadCount);
-        CyclicBarrier barrier = new CyclicBarrier(threadCount, new Runnable() {
-            int count = 1;
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+        AtomicInteger counter = new AtomicInteger(1);
 
-            @Override
-            public void run() {
-                if (count <= 100) {
-                    System.out.println("Sayı: " + count);
-                    count++;
-                }
-            }
-        });
-
-        for (int i = 0; i < threadCount; i++) {
-            sabitHavuz.execute(() -> {
-                while (true) {
+        for (int i = 0; i < MAX_NUMBER; i++) {
+            int num = i + 1;
+            threadPool.execute(() -> {
+                synchronized (counter) {
+                    while (counter.get() != num) {
+                        try {
+                            counter.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
                     try {
                         Thread.sleep(100);
-                        barrier.await();
-                    } catch (InterruptedException | BrokenBarrierException e) {
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
-                        break;
                     }
+                    System.out.println("Sayı: " + num );
+                    counter.incrementAndGet();
+                    counter.notifyAll();
                 }
             });
         }
 
-        sabitHavuz.shutdown();
+        threadPool.shutdown();
     }
 }
